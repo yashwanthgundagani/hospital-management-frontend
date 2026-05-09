801c5fa886321524a01e7283d85742b3190f0995
Yashwanth Gundagani <yashwanthgundagani@gmail.com>
2026-05-09 16:26:28 +0530

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  standalone: false,
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('frontend-hospital-mgmt-sys');
+}
